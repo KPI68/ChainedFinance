28b565e0c8af67b7c9e01550736605e928dbbf1a
KPI68 <pi_kala@yahoo.ca>
2023-10-23 21:57:19 -0400

    change contract to enable renewal

--- contracts/loan.sol
+++ contracts/loan.sol
@@ -4,33 +4,53 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 
 contract LoanRegistry is ERC721Full {
     constructor() public ERC721Full("LoanToken", "LOAN") {}
+    struct Loan {
+        string start_date;
+        uint tenor;    
+        string loan_uri;    
+    }
+    mapping(uint256 => Loan) public loanCollection;
 
-    mapping(uint256 => string) public loanCollection;
-
-    event Exposed(uint256 tokenId, string detailsJSON);
+    event Exposed(uint256 tokenId, string loan_uri);
 
     function getLoanDetails(uint256 tokenId) public view returns (string memory) {
-        return loanCollection[tokenId];
+        return loanCollection[tokenId].loan_uri;
+    }
+
+    function getLoanStartDate(uint256 tokenId) public view returns (string memory) {
+        return loanCollection[tokenId].start_date;
+    }
+
+    function getLoanTenor(uint256 tokenId) public view returns (uint) {
+        return loanCollection[tokenId].tenor;
+    }
+
+    function setLoanStartDate(uint256 tokenId, string memory start_date) public {
+        loanCollection[tokenId].start_date = start_date;
+    }   
+
+    function setLoanTenor(uint256 tokenId, uint tenor) public {
+        loanCollection[tokenId].tenor = tenor;
     }
 
     function registerLoan(
-        string memory loan_url
-    ) public returns (uint256) {
+        string memory start_date,
+        uint tenor,
+        string memory loan_uri
+    ) public {
         uint256 tokenId = totalSupply();
 
         _mint(msg.sender, tokenId);
-        _setTokenURI(tokenId, loan_url);
+        _setTokenURI(tokenId, loan_uri);
 
-        loanCollection[tokenId] = loan_url;
-
-        return tokenId;
+        loanCollection[tokenId] = Loan(start_date, tenor, loan_uri);
     }    
 
     function exposeDelinquent(
         uint256 tokenId
     ) public returns (string memory) {
-        emit Exposed(tokenId, loanCollection[tokenId]);
+        emit Exposed(tokenId, loanCollection[tokenId].loan_uri);
 
-        return loanCollection[tokenId];
+        return loanCollection[tokenId].loan_uri;
     }
 } 
