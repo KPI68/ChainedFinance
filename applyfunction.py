fc084b31b22dd7122bfb38e9474b95e010f347bc
KPI68 <pi_kala@yahoo.ca>
2023-10-24 19:57:04 -0400

    almost v1

--- applyfunction.py
+++ applyfunction.py
@@ -4,9 +4,16 @@ import numpy as np
 from datetime import date
 from pinata import pin_file_to_ipfs, pin_json_to_ipfs, convert_data_to_json
 
+education_mapping = {'High School': 1, 'Diploma': 2, 'Bachelor': 3, 'Master': 4, 'PhD': 5}
+loan_purpose_mapping = {'Home': 1, 'Auto': 2, 'Education': 3, 'Personal': 4}
 
-def pin_loan_data(account_address, loan_details, file):
-    ipfs_file_hash = pin_file_to_ipfs(file.getvalue())
+def pin_loan_data(account_address, loan_details, file, file_hash=None):
+    if file != None and file_hash == None:
+        ipfs_file_hash = pin_file_to_ipfs(file.getvalue())
+
+    if file_hash != None:
+        ipfs_file_hash = file_hash
+
     token_json = {
         "name": account_address,
         "details": loan_details,
@@ -16,8 +23,27 @@ def pin_loan_data(account_address, loan_details, file):
     json_ipfs_hash = pin_json_to_ipfs(json_data)
     return json_ipfs_hash, token_json
 
-def loan_appraisal(account_address, loan_amount_cad, loan_term_days):
+def ml_decision(loan_amount_cad, loan_term_days, details):
     gb_model = joblib.load('gb_model.pkl')
+    age = details['age']
+    income = details['income']
+    employment_years = details['employment_years']
+    education_level = details['education_level']
+    education_level_num = education_mapping[education_level]
+    loan_purpose = details['loan_purpose']
+    loan_purpose_encoded = loan_purpose_mapping[loan_purpose]
+    credit_score = details['credit_score']
+    open_accounts = details['open_accounts']
+    outstanding_debt = details['outstanding_debt']
+    dti = details['dti']
+    input_features = np.array([age, income, employment_years, education_level_num, loan_purpose_encoded,
+                                credit_score, loan_amount_cad, loan_term_days, open_accounts, outstanding_debt, dti])
+
+    # predictions using gb_model.
+    gb_prediction = gb_model.predict(input_features.reshape(1, -1))
+    return gb_prediction[0]==1
+
+def loan_appraisal(account_address, loan_amount_cad, loan_term_days):
 
     st.title('Loan Application and Credit Worthiness Prediction')
 
@@ -42,23 +68,13 @@ def loan_appraisal(account_address, loan_amount_cad, loan_term_days):
 
     # Encode 'Education Level'
     education_level = st.selectbox('Education Level', ['High School', 'Diploma', 'Bachelor', 'Master', 'PhD'])
-    education_mapping = {'High School': 1, 'Diploma': 2, 'Bachelor': 3, 'Master': 4, 'PhD': 5}
-    education_level_num = education_mapping[education_level]
 
     # Encode 'Loan Purpose'
     loan_purpose = st.selectbox('Loan Purpose', ['Home', 'Auto', 'Education', 'Personal'])
-    loan_purpose_mapping = {'Home': 1, 'Auto': 2, 'Education': 3, 'Personal': 4}
-    loan_purpose_encoded = loan_purpose_mapping[loan_purpose]
 
     credit_score = st.number_input('Credit Score', min_value=300, max_value=900)
     open_accounts = st.number_input('Number of Open Accounts', min_value=0)
     outstanding_debt = st.number_input('Outstanding Debt', min_value=0)
-    #loan_amount_eth = st.number_input('Loan Amount in ETH', min_value=0)
-    #loan_term_days = st.number_input('Loan Term in Days', min_value=1, max_value=366)
-
-    # convert ETH input into CAD value before passing to ML model.
-    #eth_to_cad_rate = get_CADR()
-    #loan_amount_cad = loan_amount_eth * eth_to_cad_rate
 
     dti = round(outstanding_debt / income * 100, 2)
 
@@ -68,36 +84,31 @@ def loan_appraisal(account_address, loan_amount_cad, loan_term_days):
     
     #  button to make the predictions
     if st.button('Submit'):
-        input_features = np.array([age, income, employment_years, education_level_num, loan_purpose_encoded,
-                                   credit_score, loan_amount_cad, loan_term_days, open_accounts, outstanding_debt, dti])
-
-        # predictions using gb_model.
-        gb_prediction = gb_model.predict(input_features.reshape(1, -1))
-        #gb_prediction = [1]
-
-        if gb_prediction[0] == 1:
+        dob_str = str(dob)
+        loan_details = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'dob': dob_str,
+            'government_id_number': government_id_number,
+            'age': age,
+            'income': income,
+            'outstanding_debt': outstanding_debt,
+            'dti': dti,
+            'employment_years': employment_years,
+            'education_level': education_level,
+            'loan_purpose': loan_purpose,
+            'credit_score': credit_score,
+            'open_accounts': open_accounts,
+            'outstanding_debt': outstanding_debt
+        }
+        if ml_decision(loan_amount_cad, loan_term_days, loan_details):
             st.write("Loan Approved")
-            dob_str = str(dob)
-            loan_details = {
-                'first_name': first_name,
-                'last_name': last_name,
-                'dob': dob_str,
-                'government_id_number': government_id_number,
-                'age': age,
-                'income': income,
-                'outstanding_debt': outstanding_debt,
-                'dti': dti,
-                'employment_years': employment_years,
-                'education_level': education_level,
-                'loan_purpose': loan_purpose,
-                'credit_score': credit_score,
-                'loan_amount_cad': loan_amount_cad
-            }
+
             try:
                 loan_ipfs_hash, token_json = pin_loan_data(account_address, loan_details, file)
                 loan_uri = f"ipfs://{loan_ipfs_hash}"
                 st.markdown(f"[Loan details Link](https://ipfs.io/ipfs/{loan_ipfs_hash})")
-                return loan_details, loan_uri
+                return loan_uri
             except AttributeError:
                 st.write("file null")
                 return None
@@ -105,33 +116,69 @@ def loan_appraisal(account_address, loan_amount_cad, loan_term_days):
             st.write("Loan Declined")
             return None
 
-def loan_renewal(details, tenor):
-    gb_model = joblib.load('gb_model.pkl')
-    #display details for renewal
-    if details != None:
-        age = details['age']
-        income = details['income']
-        employment_years = details['employment_years']
-        education_level = details['education_level']
-        education_mapping = {'High School': 1, 'Diploma': 2, 'Bachelor': 3, 'Master': 4, 'PhD': 5}
-        education_level_num = education_mapping[education_level]
-        loan_purpose = details['loan_purpose']
-        loan_purpose_mapping = {'Home': 1, 'Auto': 2, 'Education': 3, 'Personal': 4}
-        loan_purpose_encoded = loan_purpose_mapping[loan_purpose]
-        credit_score = details['credit_score']
-        open_accounts = details['open_accounts']
-        outstanding_debt = details['outstanding_debt']
-        dti = details['dti']
-        loan_amount_cad = details['loan_amount_cad']
-        loan_term_days = tenor
-        input_features = np.array([age, income, employment_years, education_level_num, loan_purpose_encoded,
-                                   credit_score, loan_amount_cad, loan_term_days, open_accounts, outstanding_debt, dti])
-        # predictions using gb_model.
-        gb_prediction = gb_model.predict(input_features.reshape(1, -1))
-        if gb_prediction[0] == 1:
-            st.write("Renewal Approved")
-            return True
+def loan_renewal(account_address, loan_amount_cad, loan_term_days, details, img_hash):
+
+    st.title('Loan Application and Credit Worthiness Prediction')
+
+    st.header('Customer Information')
+
+    # Collect personal information
+    st.write(f"First Name: {details['first_name']}")
+    st.write(f"Last Name: {details['last_name']}")
+    st.write(f"Date of Birth: {details['dob']}")
+    st.write(f"Government ID: {details['government_id_number']}")
+
+    # Calculate age based on the provided DOB
+    today = date.today()
+    dob = date.fromisoformat(details['dob'])
+    if dob:
+        age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+    else:
+        age = None
+
+    # Financial information
+    income = st.number_input('Income', min_value=1, value=details['income'])
+    employment_years = st.number_input('Years of Employment', min_value=0, value=details['employment_years'])
+
+    # Encode 'Education Level'
+    education_level = st.selectbox('Education Level', 
+                                   ['High School', 'Diploma', 'Bachelor', 'Master', 'PhD'],
+                                   index=education_mapping[details['education_level']])
+
+    # Encode 'Loan Purpose'
+    loan_purpose = st.selectbox('Loan Purpose', 
+                                ['Home', 'Auto', 'Education', 'Personal'],
+                                index=loan_purpose_mapping[details['loan_purpose']])
+
+    credit_score = st.number_input('Credit Score', min_value=300, max_value=900, value=details['credit_score'])
+    open_accounts = st.number_input('Number of Open Accounts', min_value=0, value=details['open_accounts'])
+    outstanding_debt = st.number_input('Outstanding Debt', min_value=0, value=details['outstanding_debt'])
+
+    dti = round(outstanding_debt / income * 100, 2)
+
+    # Register New Loan
+    #  button to make the predictions
+    if st.button('Submit'):
+        loan_details = details
+        loan_details['age'] = age
+        loan_details['income'] = income
+        loan_details['outstanding_debt'] = outstanding_debt
+        loan_details['dti'] = dti
+        loan_details['employment_years'] = employment_years
+        loan_details['education_level'] = education_level
+        loan_details['loan_purpose'] = loan_purpose
+        loan_details['credit_score'] = credit_score
+        loan_details['open_accounts'] = open_accounts
+        loan_details['outstanding_debt'] = outstanding_debt
+    
+        if ml_decision(loan_amount_cad, loan_term_days, loan_details):
+            st.write("Loan Approved")
+
+            loan_ipfs_hash, token_json = pin_loan_data(account_address, loan_details, None, img_hash)
+            loan_uri = f"ipfs://{loan_ipfs_hash}"
+            st.markdown(f"[Loan details Link](https://ipfs.io/ipfs/{loan_ipfs_hash})")
+            return loan_uri
         else:
-            st.write("Renewal Declined")
-            return False
+            st.write("Loan Declined")
+            return None
 
--- contracts/loan.sol
+++ contracts/loan.sol
@@ -11,27 +11,21 @@ contract LoanRegistry is ERC721Full {
     }
     mapping(uint256 => Loan) public loanCollection;
 
-    event Exposed(uint256 tokenId, string loan_uri);
-
-    function getLoanDetails(uint256 tokenId) public view returns (string memory) {
-        return loanCollection[tokenId].loan_uri;
+    function getLoanDetails(uint256 tokenId) public view returns (string memory, uint, string memory) {
+        return (loanCollection[tokenId].start_date,
+                loanCollection[tokenId].tenor,
+                loanCollection[tokenId].loan_uri);
     }
 
-    function getLoanStartDate(uint256 tokenId) public view returns (string memory) {
-        return loanCollection[tokenId].start_date;
-    }
+    function updateLoan(
+        uint256 tokenId,
+        string memory start_date,
+        uint tenor,
+        string memory loan_uri
+    ) public {
 
-    function getLoanTenor(uint256 tokenId) public view returns (uint) {
-        return loanCollection[tokenId].tenor;
-    }
-
-    function setLoanStartDate(uint256 tokenId, string memory start_date) public {
-        loanCollection[tokenId].start_date = start_date;
-    }   
-
-    function setLoanTenor(uint256 tokenId, uint tenor) public {
-        loanCollection[tokenId].tenor = tenor;
-    }
+        loanCollection[tokenId] = Loan(start_date, tenor, loan_uri);
+    }    
 
     function registerLoan(
         string memory start_date,
@@ -45,12 +39,4 @@ contract LoanRegistry is ERC721Full {
 
         loanCollection[tokenId] = Loan(start_date, tenor, loan_uri);
     }    
-
-    function exposeDelinquent(
-        uint256 tokenId
-    ) public returns (string memory) {
-        emit Exposed(tokenId, loanCollection[tokenId].loan_uri);
-
-        return loanCollection[tokenId].loan_uri;
-    }
 } 
